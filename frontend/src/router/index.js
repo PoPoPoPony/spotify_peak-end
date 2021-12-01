74829667b3518148e6461f516a56017ec627daff
pony <pony8674312@gmail.com>
2021-12-02 00:47:20 +0800

    add create_list add new font

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import login from '@/views/login'
 import tags from '@/views/tags'
 import song_list from '@/views/song_list'
 import list_credit from '@/views/list_credit'
+import create_list from '@/views/create_list'
 
 const routes = [
 	{
@@ -24,6 +25,11 @@ const routes = [
 		path: '/list_credit',
 		name: 'list_credit',
 		component: list_credit
+	},
+	{
+		path: '/create_list',
+		name: 'create_list',
+		component: create_list
 	}
 
 ]
